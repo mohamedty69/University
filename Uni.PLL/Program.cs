fe4e540b0d60fb8cf8a878adfd1f47a6a0446ff7
eima40x4c <imalwaysforlife@gmail.com>
2025-05-14 14:30:56 +0300

    AdminDashboard & LoginPage working as expected

--- Uni.PLL/Program.cs
+++ Uni.PLL/Program.cs
@@ -37,7 +37,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 // Add these middleware in correct order
-app.UseAuthentication(); // You were missing this!
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -45,51 +45,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
-//using System;
-//using Uni.DAL.Entity;
-//using Uni.DAL.DB;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.AspNetCore.Identity;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddControllersWithViews();
-
-//var app = builder.Build();
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//               options.UseSqlServer("name=DefaultConnection"));
-
-//builder.Services.AddIdentity<Student, IdentityRole>()
-//                .AddEntityFrameworkStores<AppDbContext>()
-//                .AddDefaultTokenProviders();
-
-
-
-//builder.Services.AddIdentityCore<Student>(options => options.SignIn.RequireConfirmedAccount = true)
-//                .AddRoles<IdentityRole>()
-//                .AddEntityFrameworkStores<AppDbContext>()
-//                .AddTokenProvider<DataProtectorTokenProvider<Student>>(TokenOptions.DefaultProvider);
-
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//app.Run();
